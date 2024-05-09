29e7d0dcb4b3dfd12535548d897289f173005c90
Rashid Ayoupov <valentaine1234@gmail.com>
2024-05-10 02:26:55 +0300

    add users, tags, favorite,

--- backend/news/api_v1/urls.py
+++ backend/news/api_v1/urls.py
@@ -1,16 +1,22 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api_v1.views import PostViewSet
+from api_v1.views import PostViewSet, TagViewSet, add_favorite
+from users.views import SignUp
 
 app_name = "api_v1"
 
 router = DefaultRouter()
 
+router.register("users", SignUp, basename="users"),
 router.register("posts", PostViewSet, basename="posts")
+router.register("tags", TagViewSet, basename="tags")
 
 urlpatterns = [
+    path(
+        "posts/<int:post_id>/add_favorite/", add_favorite, name="add_favorite"
+    ),
     path("", include(router.urls)),
-    # path('', include('djoser.urls')),
-    # path('auth/', include('djoser.urls.authtoken')),
+    path("", include("djoser.urls")),
+    path("auth/", include("djoser.urls.authtoken")),
 ]
