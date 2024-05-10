854b8a3788b5f68bd92ae4a2dfb5036eea4efd61
Rashid Ayoupov <valentaine1234@gmail.com>
2024-05-11 00:58:16 +0300

    doc add

--- backend/news/api_v1/views.py
+++ backend/news/api_v1/views.py
@@ -1,15 +1,21 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import viewsets
 from rest_framework.decorators import api_view, permission_classes, action
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import (
     IsAuthenticatedOrReadOnly,
     IsAuthenticated,
+    SAFE_METHODS,
 )
 from rest_framework.response import Response
 
-from api_v1.serializers import TagSerializer, PostSerializer
+from api_v1.serializers import (
+    TagSerializer,
+    PostSerializer,
+    PostEditSerializer,
+)
 from posts.models import Tag, Post, Favorites
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
@@ -18,18 +24,54 @@ from .filters import PostFilter
 from .permissions import ReadOnly
 
 
+@extend_schema(
+    tags=["Теги"],
+    methods=["GET"],
+    description="Все пользователи",
+)
+@extend_schema_view(
+    list=extend_schema(
+        summary="Получить список тегов",
+    ),
+    retrieve=extend_schema(
+        summary="Детальная информация о теге",
+    ),
+)
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     permission_classes = [ReadOnly]
+    http_method_names = ["get"]
 
 
+@extend_schema(
+    tags=["Посты"],
+    methods=["GET", "POST", "DELETE"],
+    description="Все пользователи",
+)
+@extend_schema_view(
+    list=extend_schema(
+        summary="Получить список постов",
+    ),
+    retrieve=extend_schema(
+        summary="Детальная информация о посте",
+    ),
+)
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
     filterset_class = PostFilter
     pagination_class = LimitOffsetPagination
+    http_method_names = ["delete", "post", "get"]
+
+    def get_serializer_class(self):
+        if self.request.method in SAFE_METHODS:
+            return PostSerializer
+        return PostEditSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
     @action(detail=True, methods=["get"])
     def view_post(self, request, pk=None):
@@ -39,6 +81,11 @@ class PostViewSet(viewsets.ModelViewSet):
         return Response({"message": "Просмотр поста увеличен на 1."})
 
 
+@extend_schema(
+    tags=["Лайки"],
+    methods=["GET", "POST", "DELETE"],
+    description="Все пользователи",
+)
 @api_view(["POST", "DELETE"])
 @permission_classes([IsAuthenticated])
 def add_favorite(request, post_id):
