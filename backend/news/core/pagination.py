27a481a1220d0956e5da868136ef23d2e738f31a
Rashid Ayoupov <valentaine1234@gmail.com>
2024-05-11 00:53:52 +0300

    delete

--- backend/news/core/pagination.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-from django.conf import settings
-
-
-class LargeResultsSetPagination(PageNumberPagination):
-    page_size = settings.PAGE_SIZE
-    page_size_query_param = "page_size"
